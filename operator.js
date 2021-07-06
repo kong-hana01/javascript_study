8c7539d859a92e54f7db6174cde665474e641d1c
konghana <gksqlsl11@khu.ac.kr>
2021-07-06 23:47:00 +0900

    반복문(while, do while, for), 조건문(if, ?, switch) 학습 완료

--- operator.js
+++ operator.js
@@ -107,3 +107,88 @@ console.log('' == false); // true
 console.log('' === false); // false
 console.log(null == undefined); // true
 console.log(null === undefined); // false
+
+// 8. Conditional operators: if
+// if, else if, else
+const name = 'coder';
+if (name === 'coder') {
+    console.log('Welcome Hanbin');
+} else if (name === 'coder') {
+    console.log('You are amazing coder'); // 동작
+} else {
+    console.log('unkwnon');
+}
+
+// tenrary operator: ?
+// condition ? value1 : value2;
+console.log(name === 'ellie' ? 'yes' : 'no')
+
+// 10. Switch statement
+// use for multiple if checks
+// use for enum-like value check
+// use for multiple type checks in TS
+const browser = 'IE';
+switch (browser) {
+    case 'IE':
+        console.log('go away');
+        break;
+    case 'Chrome':
+    case 'Firefox': // 위와 같이 두 케이스에 모두 같은 값을 할당한다면 연달아서 사용하면 된다.
+        console.log('love you'); 
+        break;
+    default:
+        console.log('same all!');
+        break;
+}
+
+// 11. Loops
+// while loop, while the condition is truthy.
+// body code is executed.
+let i = 3;
+while (i > 0) {
+    console.log(`while ${i}`);
+    i--;
+}
+
+// do while loop, body code is excuted first
+// then check the condition.
+do {
+    console.log(`do while: ${i}`);
+    i--;
+} while (i > 0);
+
+// 12. for loop, for(begin; condition; step)
+for (i = 3; i > 0; i--) {
+    console.log(`for: ${i}`);
+}
+
+for (let i = 3;  i < 10; i = i + 2) {
+    // inline variable declaration
+    console.log(`for: ${i}`);
+}
+
+// nested loops
+for (let i = 0; i < 10; i++){
+    for (let j = 0; j < 10; j++){
+        console.log(`i: ${i}, j:${j}`);
+    }
+}
+
+// break continue
+// Q1. iterate from 0 to 10 and pring only even numbers (use continue)
+console.log('Q1')
+for (i = 0; i <= 10; i++){
+    if (i % 2 == 1) {
+        continue;
+    }
+    console.log(`${i}`)
+}
+
+// Q2. iterate from 0 to 10 and pring numbers until reaching 8 (use break)
+console.log('\nQ2')
+for (i = 0; i < 10; i++){
+    if (i == 8) {
+        break;
+    }
+    console.log(`${i}`);
+}
